8db32ceb960551ca18c341374898ef2def463fb8
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-01-04 12:08:15 +0530

    init

--- Home Work/Backtracking/letter_case_permutation.java
+++ Home Work/Backtracking/letter_case_permutation.java
@@ -1,56 +1,58 @@
+import java.util.List;
+
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        
-        if(s.length()==0){
-            
+
+        if (s.length() == 0) {
+
             List<String> bres = new ArrayList<>();
             bres.add("");
             return bres;
         }
-        
+
         char ch = s.charAt(0);
         List<String> mres = new ArrayList<>();
-        
-        if(Character.isDigit(ch)){
-            
+
+        if (Character.isDigit(ch)) {
+
             List<String> rres = letterCasePermutation(s.substring(1));
-            
-            for(String str : rres){
-                
-                mres.add(ch+str);
+
+            for (String str : rres) {
+
+                mres.add(ch + str);
             }
         }
-        
-        else{
-            
+
+        else {
+
             List<String> rres = letterCasePermutation(s.substring(1));
-            
-            for(String s1 : rres){
-                
-                mres.add(ch+s1);
+
+            for (String s1 : rres) {
+
+                mres.add(ch + s1);
             }
-            
-            if(ch>='a' && ch<='z'){
-                
-                ch = (char)(ch-32);
-                for(String str : rres){
-                    
-                    mres.add(ch+str);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                ch = (char) (ch - 32);
+                for (String str : rres) {
+
+                    mres.add(ch + str);
                 }
             }
-            
-            else if(ch>='A' && ch<='Z'){
-                
-                ch = (char)(ch+32);
-                
-                for(String str : rres){
-                    
-                    mres.add(ch+str);
+
+            else if (ch >= 'A' && ch <= 'Z') {
+
+                ch = (char) (ch + 32);
+
+                for (String str : rres) {
+
+                    mres.add(ch + str);
                 }
             }
-            
+
         }
-        
+
         return mres;
     }
 }
