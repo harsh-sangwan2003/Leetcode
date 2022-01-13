1a61be65f2c3d599726311599b391103aec24742
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-01-13 11:43:44 +0530

    init

--- Linked List/Day - 8/design_linked_list.java
+++ Linked List/Day - 8/design_linked_list.java
@@ -1,140 +1,129 @@
 class MyLinkedList {
-
-    /** Initialize your data structure here. */
-
-    public class Node {
-
+    
+    public class Node{
+        
         int data;
         Node next;
-
-        public Node(int val) {
-
+        
+        public Node(int val){
+            
             this.data = val;
         }
-
     }
-
+    
     private Node head;
     private int size;
-
+    
     public MyLinkedList() {
-
+        
     }
-
-    /**
-     * Get the value of the index-th node in the linked list. If the index is
-     * invalid, return -1.
-     */
+    
     public int get(int index) {
-
-        if (index < 0 || index >= size)
+        
+        if(index<0 || index>=size)
             return -1;
-
+        
         Node temp = head;
-
-        for (int i = 0; i < index; i++) {
+        
+        for(int i=0; i<index; i++)
             temp = temp.next;
-        }
-
+        
         return temp.data;
     }
-
-    /**
-     * Add a node of value val before the first element of the linked list. After
-     * the insertion, the new node will be the first node of the linked list.
-     */
+    
     public void addAtHead(int val) {
-
-        if (size == 0) {
-            Node temp = new Node(val);
-            head = temp;
-            temp.next = null;
+        
+        if(size==0){
+            
+            Node node = new Node(val);
+            head = node;
+            node.next = null;
             size++;
         }
-
-        else {
-            Node temp = new Node(val);
-            temp.next = head;
-            head = temp;
+        
+        else{
+            
+            Node node = new Node(val);
+            node.next = head;
+            head = node;
             size++;
         }
     }
-
-    /** Append a node of value val to the last element of the linked list. */
+    
     public void addAtTail(int val) {
-
-        if (size == 0) {
-            Node temp = new Node(val);
-            head = temp;
-            temp.next = null;
+        
+        if(size==0){
+            
+            Node node = new Node(val);
+            head = node;
+            node.next = null;
             size++;
         }
-
-        else {
-            Node temp = head;
-
-            for (int i = 0; i < size - 1; i++)
-                temp = temp.next;
-
+        
+        else{
+            
             Node node = new Node(val);
+            Node temp = head;
+            
+            for(int i=0; i<size-1; i++)
+                temp = temp.next;
+            
             temp.next = node;
             node.next = null;
             size++;
         }
     }
-
-    /**
-     * Add a node of value val before the index-th node in the linked list. If index
-     * equals to the length of linked list, the node will be appended to the end of
-     * linked list. If index is greater than the length, the node will not be
-     * inserted.
-     */
+    
     public void addAtIndex(int index, int val) {
-
-        if (index < 0 || index > size)
+        
+        if(index<0 || index>size)
             return;
-
-        if (index == 0)
+        
+        else if(index==0){
+            
             addAtHead(val);
-
-        else if (index == size)
+        }
+        
+        else if(index==size){
+            
             addAtTail(val);
-
-        else {
+        }
+        
+        else{
+            
             Node node = new Node(val);
             Node temp = head;
-
-            for (int i = 0; i < index - 1; i++) {
+            
+            for(int i=0; i<index-1; i++)
                 temp = temp.next;
-            }
-
+            
             node.next = temp.next;
             temp.next = node;
             size++;
-
         }
     }
-
-    /** Delete the index-th node in the linked list, if the index is valid. */
+    
     public void deleteAtIndex(int index) {
-
-        if (index < 0 || index >= size)
+        
+        if(index<0 || index>=size)
             return;
-
-        if (index == 0) {
+        
+        if(index==0){
+            
             head = head.next;
+            size--;
         }
-
-        else {
+        
+        else{
+            
             Node temp = head;
-
-            for (int i = 0; i < index - 1; i++)
+            
+            for(int i=0; i<index-1; i++)
                 temp = temp.next;
-
+            
             temp.next = temp.next.next;
+            size--;
         }
-
-        size--;
     }
 }
 
