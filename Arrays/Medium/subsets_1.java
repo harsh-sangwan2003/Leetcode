096a7a64b1764718c5968bcc8ae5b1aba292e435
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-02-17 20:46:08 +0530

    added

--- Arrays/Medium/subsets_1.java
+++ Arrays/Medium/subsets_1.java
@@ -1,28 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsets(int[] arr) {
-        
+
         List<List<Integer>> res = new ArrayList<>();
-        
-        int end = (int)Math.pow(2,arr.length);
-        
-        for(int i=0; i<end; i++){
-            
+
+        int end = (int) Math.pow(2, arr.length);
+
+        for (int i = 0; i < end; i++) {
+
             int temp = i;
             List<Integer> list = new ArrayList<>();
-            
-            for(int j=0; j<arr.length; j++){
-                
-                int rem = temp%2;
-                temp/=2;
-                
-                if(rem!=0)
+
+            for (int j = 0; j < arr.length; j++) {
+
+                int rem = temp % 2;
+                temp /= 2;
+
+                if (rem != 0)
                     list.add(arr[j]);
             }
-            
+
             res.add(list);
         }
-        
+
         return res;
     }
-    
+
 }
