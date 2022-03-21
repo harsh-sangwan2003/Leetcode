7ad31bf37b3d9fbd8b616dbb3771dfa8d15d548f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-03-21 16:57:01 +0530

    added

---
+++ New-March(2022)/Array/Easy/two_sum2.java
@@ -0,0 +1,27 @@
+// Time - O(n)
+// Space - O(n)
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+
+                int left = map.get(target - nums[i]);
+                return new int[] { left, i };
+            }
+
+            else {
+
+                map.put(nums[i], i);
+            }
+        }
+
+        return new int[2];
+    }
+}
