efacafd9e5096c10d57fefa8bff50036edd0b3ac
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-03-23 18:14:54 +0530

    added

---
+++ New-March(2022)/Dynamic Programming/Easy/climb_stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        return helper(n,new int[n+1]);
+    }
+    
+    public int helper(int n, int[] dp){
+        
+        if(n==0 || n==1) return dp[n] = 1;
+        
+        if(n<0) return 0;
+        
+        if(dp[n]!=0)
+            return dp[n];
+        
+        int n_1 = helper(n-1,dp);
+        int n_2 = helper(n-2,dp);
+        
+        return dp[n] = n_1 + n_2;
+    }
+}
---
+++ New-March(2022)/Dynamic Programming/Easy/climb_stairs2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        return helper(n, new int[n + 1]);
+    }
+
+    public int helper(int N, int[] dp) {
+
+        for (int n = 0; n <= N; n++) {
+
+            if (n == 0 || n == 1) {
+                dp[n] = 1;
+                continue;
+            }
+
+            int n_1 = dp[n - 1];
+            int n_2 = dp[n - 2];
+
+            dp[n] = n_1 + n_2;
+        }
+
+        return dp[N];
+    }
+}
