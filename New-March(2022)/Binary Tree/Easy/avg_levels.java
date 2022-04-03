ae6e318ee05db60b98e93ef20a2711acf60d6035
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-03 10:07:10 +0530

    added

---
+++ New-March(2022)/Binary Tree/Easy/avg_levels.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.List;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Double> list = new ArrayList<>();
+
+        q.add(root);
+
+        while (q.size() != 0) {
+
+            Double temp = 0.0;
+            int size = q.size();
+            int ts = size;
+
+            while (size-- > 0) {
+
+                TreeNode node = q.remove();
+                temp += (double) (node.val);
+
+                if (node.left != null)
+                    q.add(node.left);
+
+                if (node.right != null)
+                    q.add(node.right);
+
+            }
+
+            list.add(temp / ts);
+        }
+
+        return list;
+    }
+}
