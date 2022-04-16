77537ea916f18e305ee13f0bfde5cbde9b53d674
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-16 16:18:36 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (13.43%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        
+        int minr = 0;
+        int minc = 0;
+        int maxr = arr.length-1;
+        int maxc = arr[0].length-1;
+        
+        int count = 0;
+        int ne = arr.length*arr[0].length;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        while(count<ne){
+            
+            //Top
+            for(int c=minc; c<=maxc && count<ne; c++){
+                
+                list.add(arr[minr][c]);
+                count++;
+            }
+            minr++;
+            
+            //Right
+            for(int r=minr; r<=maxr && count<ne; r++){
+                
+                list.add(arr[r][maxc]);
+                count++;
+            }
+            maxc--;
+            
+            //Bottom
+            for(int c=maxc; c>=minc && count<ne; c--){
+                
+                list.add(arr[maxr][c]);
+                count++;
+            }
+            maxr--;
+            
+            //Left
+            for(int r=maxr; r>=minr && count<ne; r--){
+                
+                list.add(arr[r][minc]);
+                count++;
+            }
+            minc++;
+        }
+        
+        return list;
+    }
+}
