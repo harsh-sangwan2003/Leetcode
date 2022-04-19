f223172e1b7eafeaff90091a80b1bae16a97c62a
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-19 10:59:45 +0530

    Time: 1 ms (69.87%), Space: 43.3 MB (30.34%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,36 +1,54 @@
-//   Definition for singly-linked list.
-// class ListNode {
-//     int val;
-//     ListNode next;
-
-//     ListNode(int x) {
-//         val = x;
-//     }
-// }
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
-        if (l1 == null)
-            return l2;
-
-        if (l2 == null)
-            return l1;
-
-        ListNode node = null;
-
-        if (l1.val < l2.val) {
-
-            node = new ListNode(l1.val);
-            node.next = mergeTwoLists(l1.next, l2);
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        
+        while(l1!=null && l2!=null){
+            
+            if(l1.val<l2.val){
+                
+                ListNode node = new ListNode(l1.val);
+                temp.next = node;
+                l1 = l1.next;
+            }
+            
+            else{
+                
+                ListNode node = new ListNode(l2.val);
+                temp.next = node;
+                l2 = l2.next;
+            }
+            
+            temp = temp.next;
         }
-
-        else {
-
-            node = new ListNode(l2.val);
-            node.next = mergeTwoLists(l1, l2.next);
+        
+        while(l1!=null){
+            
+            ListNode node = new ListNode(l1.val);
+            temp.next = node;
+            l1 = l1.next;
+            temp = temp.next;
         }
-
-        return node;
+        
+        while(l2!=null){
+            
+            ListNode node = new ListNode(l2.val);
+            temp.next = node;
+            l2 = l2.next;
+            temp = temp.next;
+        }
+        
+        return dummy.next;
     }
 }
