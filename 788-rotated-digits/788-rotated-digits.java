0ba30faee6e9211addfbbf759655d7841b3f6a38
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-20 20:38:42 +0530

    Time: 6 ms (64.33%), Space: 39.3 MB (90.06%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.java
+++ 788-rotated-digits/788-rotated-digits.java
@@ -1,44 +1,43 @@
 class Solution {
+    
+    public boolean contains2569(int num){
+        
+        while(num!=0){
+            
+            int rem = num%10;
+            
+            if(rem==2 || rem==5 || rem==6 || rem==9) return true;
+            
+            num/=10;
+        }
+        
+        return false;
+    }
+    
+    public boolean contains347(int num){
+        
+        while(num!=0){
+            
+            int rem = num%10;
+            
+            if(rem==3 || rem==4 || rem==7) return true;
+            
+            num/=10;
+        }
+        
+        return false;
+    }
+    
     public int rotatedDigits(int n) {
-
+        
         int count = 0;
-
-        for (int x = 1; x <= n; x++) {
-
-            if (contains2569(x) && !contains347(x))
+        
+        for(int x=1; x<=n; x++){
+            
+            if(contains2569(x) && !contains347(x))
                 count++;
         }
-
+        
         return count;
-    }
-
-    public boolean contains2569(int num) {
-
-        while (num != 0) {
-
-            int rem = num % 10;
-
-            if (rem == 2 || rem == 6 || rem == 9 || rem == 5)
-                return true;
-
-            num /= 10;
-        }
-
-        return false;
-    }
-
-    public boolean contains347(int num) {
-
-        while (num != 0) {
-
-            int rem = num % 10;
-
-            if (rem == 3 || rem == 4 || rem == 7)
-                return true;
-
-            num /= 10;
-        }
-
-        return false;
     }
 }
