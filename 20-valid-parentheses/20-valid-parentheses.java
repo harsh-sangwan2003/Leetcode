595d55c22cd58297bef951a054ab95540701b4b6
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-20 20:23:26 +0530

    Time: 5 ms (10.21%), Space: 41.9 MB (57.86%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -2,34 +2,37 @@ class Solution {
     public boolean isValid(String s) {
         
         Stack<Character> st = new Stack<>();
-        boolean flag = true;
+        boolean flag = false;
         
         for(int i=0; i<s.length(); i++){
             
             char ch = s.charAt(i);
             
-            if(ch=='(' || ch=='[' || ch=='{')
+            if(ch=='(' || ch=='{' || ch=='[')
                 st.push(ch);
             
             else if(ch==')'){
                 
                 flag = check(st,'(');
                 
-                if(!flag) return false;
+                if(!flag)
+                    return false;
             }
             
             else if(ch=='}'){
                 
                 flag = check(st,'{');
                 
-                if(!flag) return false;
+                if(!flag)
+                    return false;
             }
             
             else if(ch==']'){
                 
                 flag = check(st,'[');
                 
-                if(!flag) return false;
+                if(!flag)
+                    return false;
             }
             
         }
