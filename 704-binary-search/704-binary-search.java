6ebb7ec49252fc1b02aaf7bd70b5cb162f1f7b99
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-21 17:11:21 +0530

    Time: 0 ms (100.00%), Space: 54 MB (61.18%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -6,16 +6,13 @@ class Solution {
         
         while(lo<=hi){
             
-            int mid = lo+(hi-lo)/2;
+            int mid = lo + (hi-lo)/2;
             
-            if(nums[mid]==target)
-                return mid;
+            if(nums[mid]==target) return mid;
             
-            else if(nums[mid]>target)
-                hi = mid-1;
+            else if(nums[mid]<target) lo = mid + 1;
             
-            else
-                lo = mid+1;
+            else hi = mid - 1;
         }
         
         return -1;
