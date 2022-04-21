da2a067a733864702cf00b19dde9509bf1b8f907
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-21 22:18:18 +0530

    Time: 7 ms (13.08%), Space: 54.6 MB (5.52%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -0,0 +1,81 @@
+class Solution {
+    
+    public class Pair{
+        
+        int vtx;
+        int clr;
+        
+        Pair(){
+            
+        }
+        
+        Pair(int vtx, int clr){
+            
+            this.vtx = vtx;
+            this.clr = clr;
+        }
+        
+    }
+    
+    public boolean isBipartite(int[][] graph) {
+        
+        int vtces = graph.length;
+        int[] visit = new int[vtces];
+        Arrays.fill(visit,-1);
+        
+        for(int v=0; v<vtces; v++){
+            
+            if(visit[v]==-1){
+                
+                boolean flag = check(graph,v,visit);
+                
+                if(!flag)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean check(int[][] graph, int v, int[] visit){
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(v,0));
+        
+        while(q.size()!=0){
+            
+            int size = q.size();
+            
+            while(size-->0){
+                
+                Pair top = q.remove();
+                
+                if(visit[top.vtx]==-1){
+                    
+                    visit[top.vtx] = top.clr;
+                }
+                
+                else{
+                    
+                    if(visit[top.vtx]==top.clr)
+                        continue;
+                    
+                    else
+                        return false;
+                }
+                
+                int src = top.vtx;
+                
+                for(int e : graph[src]){
+                    
+                    if(visit[e]==-1){
+                        
+                        q.add(new Pair(e,1-top.clr));
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
