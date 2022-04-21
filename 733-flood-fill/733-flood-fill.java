ebcbeeaf3349fece38d88f23d23c9bf5c016c041
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-21 17:27:50 +0530

    Time: 1 ms (82.05%), Space: 42.9 MB (95.00%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -3,23 +3,25 @@ class Solution {
         
         int color = arr[sr][sc];
         boolean[][] visit = new boolean[arr.length][arr[0].length];
-        helper(arr,sr,sc,color,newColor,visit);
+        
+        helper(arr,sr,sc,color,visit,newColor);
+        
         return arr;
     }
     
-    public void helper(int[][] arr, int sr, int sc, int color, int newColor, boolean[][] visit){
+    public void helper(int[][] arr, int sr, int sc, int color, boolean[][] visit, int newColor){
         
-        if(sr<0 || sc<0 || sr>=arr.length || sc>=arr[0].length || arr[sr][sc]!=color || visit[sr][sc])
+        if(sr<0 || sc<0 || sr>=arr.length || sc>=arr[0].length || visit[sr][sc] || arr[sr][sc]!=color)
             return;
         
-        visit[sr][sc] = true;
         arr[sr][sc] = newColor;
+        visit[sr][sc] = true;
         
-        helper(arr,sr-1,sc,color,newColor,visit);
-        helper(arr,sr,sc-1,color,newColor,visit);
-        helper(arr,sr+1,sc,color,newColor,visit);
-        helper(arr,sr,sc+1,color,newColor,visit);
+        helper(arr,sr-1,sc,color,visit,newColor);
+        helper(arr,sr,sc-1,color,visit,newColor);
+        helper(arr,sr+1,sc,color,visit,newColor);
+        helper(arr,sr,sc+1,color,visit,newColor);
         
-        visit[sr][sc] = false;
+        
     }
 }
