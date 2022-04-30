db2992ae7c27cb1f9dc5e8523db98b6c72bbabab
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-30 18:03:12 +0530

    Time: 2 ms (56.58%), Space: 72.5 MB (72.69%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -4,15 +4,14 @@ class Solution {
         int max_sum = Integer.MIN_VALUE;
         int currsum = 0;
         
-        for(int val : nums){
+        for(int i=0; i<nums.length; i++){
+            
+            currsum += nums[i];
+            max_sum = Math.max(currsum,max_sum);
             
             if(currsum<0)
-            {
                 currsum = 0;
-            }
             
-            currsum += val;
-            max_sum = Math.max(currsum,max_sum);
         }
         
         return max_sum;
