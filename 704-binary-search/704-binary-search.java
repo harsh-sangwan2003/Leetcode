2d4dbf87cade73246f0ee44137342bcb0b9b16c4
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-30 17:27:55 +0530

    Time: 0 ms (100.00%), Space: 54.4 MB (38.80%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -4,39 +4,18 @@ class Solution {
         int lo = 0;
         int hi = nums.length-1;
         
-        return find(nums,target,lo,hi);
-    }
-    
-    public int find(int[] nums, int target, int lo, int hi){
-        
-        if(lo>hi) return -1;
-        
-        else if(lo==hi){
+        while(lo<=hi){
             
-            if(nums[lo]==target) return lo;
+            int mid = lo + (hi-lo)/2;
             
-            return -1;
-        }
-        
-        int mid = lo + (hi-lo)/2;
-        
-        if(nums[mid]==target){
+            if(nums[mid]==target)
+                return mid;
             
-            return mid;
-        }
-        
-        else if(nums[mid]>target){
+            else if(nums[mid]<target)
+                lo = mid+1;
             
-            int left = find(nums,target,lo,mid-1);
-            
-            if(left!=-1) return left;
-        }
-        
-        else{
-            
-            int right = find(nums,target,mid+1,hi);
-            
-            if(right!=-1) return right;
+            else
+                hi = mid-1;
         }
         
         return -1;
