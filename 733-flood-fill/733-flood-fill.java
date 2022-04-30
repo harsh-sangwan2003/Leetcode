4e859b743b01d966fcea6c2ffc4973b7d06dc626
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-30 17:59:44 +0530

    Time: 15 ms (5.12%), Space: 48.6 MB (11.45%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -4,24 +4,28 @@ class Solution {
         int color = arr[sr][sc];
         boolean[][] visit = new boolean[arr.length][arr[0].length];
         
-        helper(arr,sr,sc,color,visit,newColor);
+        helper(arr,sr,sc,newColor,color,visit);
         
         return arr;
     }
     
-    public void helper(int[][] arr, int sr, int sc, int color, boolean[][] visit, int newColor){
-        
-        if(sr<0 || sc<0 || sr>=arr.length || sc>=arr[0].length || visit[sr][sc] || arr[sr][sc]!=color)
-            return;
+    static int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    
+    public void helper(int[][] arr, int sr, int sc, int newColor, int color, boolean[][] visit){
         
         arr[sr][sc] = newColor;
         visit[sr][sc] = true;
         
-        helper(arr,sr-1,sc,color,visit,newColor);
-        helper(arr,sr,sc-1,color,visit,newColor);
-        helper(arr,sr+1,sc,color,visit,newColor);
-        helper(arr,sr,sc+1,color,visit,newColor);
+        System.out.print(sr + ", " + sc);
         
+        for(int i=0; i<dir.length; i++){
+            
+            int r = sr + dir[i][0];
+            int c = sc + dir[i][1];
+            
+            if(r>=0 && c>=0 && r<arr.length && c<arr[0].length && arr[r][c]==color && !visit[r][c])
+            helper(arr,r,c,newColor,color,visit);
+        }
         
     }
 }
