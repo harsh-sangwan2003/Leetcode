71f70157daf8aed30a825da5a50e581f258cfbef
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-04-30 18:17:58 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (23.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,20 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
          
-        return helper(n,new int[n+1]);
+        int a = 1;
+        int b = 1;
+        
+        for(int i=1; i<=n; i++){
+            
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        
+        return a;
     }
     
-    public int helper(int n, int[] dp){
-        
-        if(n<=1)
-            return dp[n] = 1;
-        
-        if(dp[n]!=0)
-            return dp[n];
-        
-        int n_1 = helper(n-1,dp);
-        int n_2 = helper(n-2,dp);
-        
-        return dp[n] = n_1 + n_2;
-    }
 }
