20ede0e2724f5296262ca8f0bd6a0789629a67e8
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-05-04 17:06:54 +0530

    Time: 6 ms (79.48%), Space: 42.1 MB (81.02%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,40 +1,33 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-
+        
         Stack<Integer> st = new Stack<>();
-
-        for (String s : tokens) {
-
-            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
-
+        
+        for(String s : tokens){
+            
+            if(s.equals("+") || s.equals("-") || s.equals("/") || s.equals("*"))
+            {
                 int v2 = st.pop();
                 int v1 = st.pop();
-                st.push(calc(v1, v2, s));
-
+                st.push(calc(v1,v2,s));
             }
-
-            else {
-
+            
+            else
                 st.push(Integer.valueOf(s));
-            }
-
         }
-
+        
         return st.pop();
     }
-
-    public int calc(int v1, int v2, String s) {
-
-        if (s.equals("+"))
-            return v1 + v2;
-
-        else if (s.equals("-"))
-            return v1 - v2;
-
-        else if (s.equals("*"))
-            return v1 * v2;
-
+    
+    public int calc(int v1, int v2, String s){
+        
+        if(s.equals("+")) return v1+v2;
+        
+        else if(s.equals("-")) return v1-v2;
+        
+        else if(s.equals("*")) return v1*v2;
+        
         else
-            return v1 / v2;
+            return v1/v2;
     }
 }
