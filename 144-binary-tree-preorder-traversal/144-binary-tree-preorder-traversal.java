e329b4ccc4019333027889b469f583c0e279e53e
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-05-19 22:28:36 +0530

    Time: 0 ms (100.00%), Space: 41 MB (70.59%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -1,41 +1,59 @@
-// Definition for a binary tree node.
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-
-        List<Integer> res = new ArrayList<>();
-        helper(root, res);
-
-        return res;
-    }
-
-    public void helper(TreeNode root, List<Integer> res) {
-
-        if (root == null)
-            return;
-
-        res.add(root.val);
-
-        helper(root.left, res);
-        helper(root.right, res);
-
+        
+        List<Integer> list = new ArrayList<>();
+        
+        TreeNode curr = root;
+        
+        while(curr!=null){
+            
+            //if left is null
+            if(curr.left==null){
+                
+                list.add(curr.val);
+                curr = curr.right;
+            }
+            
+            else{
+                
+                TreeNode iop = curr.left;
+                while(iop.right!=null && iop.right!=curr){
+                    
+                    iop = iop.right;
+                }
+                
+                //iop -> Inorder Predecessor
+                if(iop.right==null){
+                    
+                    //Making a thread
+                    iop.right = curr;
+                    list.add(curr.val);
+                    curr = curr.left;
+                }
+                
+                else{
+                    
+                    iop.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+        
+        return list;
     }
 }
