6ed5a1cd1e39a2465ba188fca7b9f42ed58da444
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2022-05-19 17:06:01 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (29.12%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -1,40 +1,35 @@
-// Definition for a binary tree node.
-// class TreeNode {
-//     int val;
-//     TreeNode left;
-//     TreeNode right;
-
-//     TreeNode() {
-//     }
-
-//     TreeNode(int val) {
-//         this.val = val;
-//     }
-
-//     TreeNode(int val, TreeNode left, TreeNode right) {
-//         this.val = val;
-//         this.left = left;
-//         this.right = right;
-//     }
-// }
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-
-        List<Integer> res = new ArrayList<>();
-        helper(root, res);
-
-        return res;
+    
+        List<Integer> list = new ArrayList<>();
+        
+        dfs(root,list);
+        
+        return list;
     }
-
-    public void helper(TreeNode root, List<Integer> res) {
-
-        if (root == null)
+    
+    public void dfs(TreeNode root, List<Integer> list){
+        
+        if(root==null)
             return;
-
-        helper(root.left, res);
-        helper(root.right, res);
-
-        res.add(root.val);
+        
+        dfs(root.left,list);
+        dfs(root.right,list);
+        list.add(root.val);
     }
 }
