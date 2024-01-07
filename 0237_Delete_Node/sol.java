4c706e3a3d0c6f78350fe5db3b68f56556ef9a15
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-07 18:12:56 +0530

    added

---
+++ 0237_Delete_Node/sol.java
@@ -0,0 +1,26 @@
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
