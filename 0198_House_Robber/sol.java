b44cbffba57924bab52138bba79e4ca0d92df905
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-09 17:03:25 +0530

    added

---
+++ 0198_House_Robber/sol.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int inc = nums[0];
+        int exc = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int ninc = exc + nums[i];
+            int nexc = Math.max(inc, exc);
+
+            inc = ninc;
+            exc = nexc;
+        }
+
+        return Math.max(inc, exc);
+    }
+}
