28f8f5176ed99858ac6cea65007f6cea30bb2dec
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-16 19:46:52 +0530

    added

--- 0142_LL_Cycle_2/sol.java
+++ 0142_LL_Cycle_2/sol.java
@@ -1,24 +1,27 @@
-
 //   Definition for singly-linked list.
-  class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) {
-          val = x;
-          next = null;
-      }
-  }
 
- class Solution {
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
     public ListNode detectCycle(ListNode head) {
-        
+
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) break;
+            if (slow == fast)
+                break;
         }
-        if (fast == null || fast.next == null) return null;
+        if (fast == null || fast.next == null)
+            return null;
         while (head != slow) {
             head = head.next;
             slow = slow.next;
