eb94d29d1f17a75034eb1b6e5a5711b53726c787
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-17 17:55:18 +0530

    added

---
+++ 0041_First_Missing_Positive/sol.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int val : nums) {
+
+            if (val > 0)
+                set.add(val);
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+
+            if (!set.contains(i))
+                return i;
+        }
+
+        return nums.length + 1;
+    }
+}
