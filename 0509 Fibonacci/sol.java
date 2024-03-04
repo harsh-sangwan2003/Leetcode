f95b50cd9fb085cb13691c242ec575d36880bedb
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-04 20:44:44 +0530

    added

---
+++ 0509 Fibonacci/sol.java
@@ -0,0 +1,16 @@
+class sol {
+
+    public int fib(int n) {
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return a;
+    }
+}
