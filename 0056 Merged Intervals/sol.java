e60c91770f9bfaa8f23bffb6a5b8c010dc0b3c7e
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-05 21:54:19 +0530

    added

---
+++ 0056 Merged Intervals/sol.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class sol {
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int totalIntervals = intervals.length;
+
+        int intervalStart = intervals[0][0];
+        int intervalEnd = intervals[0][1];
+
+        List<int[]> mergedList = new ArrayList<>();
+
+        for (int i = 1; i < totalIntervals; i++) {
+
+            if (intervals[i][0] > intervalEnd) {
+                mergedList.add(new int[] { intervalStart, intervalEnd });
+
+                intervalStart = intervals[i][0];
+                intervalEnd = intervals[i][1];
+            } else {
+                intervalEnd = Math.max(intervalEnd, intervals[i][1]);
+            }
+        }
+        mergedList.add(new int[] { intervalStart, intervalEnd });
+
+        return mergedList.toArray(new int[mergedList.size()][]);
+    }
+}
