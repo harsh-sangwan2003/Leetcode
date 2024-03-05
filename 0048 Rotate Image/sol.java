6bfd6b141ba43267ab7f85e11991c8b22f27e74c
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-05 21:52:18 +0530

    addedd

---
+++ 0048 Rotate Image/sol.java
@@ -0,0 +1,36 @@
+class sol {
+
+    private void reverse(int[] arr){
+
+        int lo = 0, hi = arr.length-1;
+
+        while(lo<=hi){
+
+            int temp = arr[lo];
+            arr[lo] = arr[hi];
+            arr[hi] = temp;
+
+            lo++;
+            hi--;
+        }
+    }
+    
+    public void rotate(int[][] matrix) {
+        
+        for(int i=0; i<matrix.length; i++){
+
+            for(int j=0; j<matrix[0].length; j++){
+
+                if(i<j){
+
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        for(int[] arr : matrix)
+        reverse(arr);
+    }
+}
