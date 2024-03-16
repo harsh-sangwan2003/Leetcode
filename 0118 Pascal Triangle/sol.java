8773da62536db627a22b57e2870111714232b0dd
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-03-16 21:24:11 +0530

    added

---
+++ 0118 Pascal Triangle/sol.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class sol {
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        res.add(list);
+
+        for (int i = 2; i <= numRows; i++) {
+
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+
+            for (int j = 1; j < list.size(); j++)
+                temp.add(list.get(j) + list.get(j - 1));
+
+            temp.add(1);
+            list = temp;
+            res.add(temp);
+        }
+
+        return res;
+    }
+}
